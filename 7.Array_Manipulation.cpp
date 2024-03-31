ae01dac309f070f0b97be144b9a63a5dd835126d
Mahbubur Rahman <mrahman623a@gmail.com>
2024-04-01 00:02:08 +0600

    updated

--- 7.Array_Manipulation.cpp
+++ 7.Array_Manipulation.cpp
@@ -8,31 +8,6 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-#define nl cout << endl
-#define Yes printf("Yes\n")
-#define No printf("No\n")
-#define YES printf("YES\n")
-#define NO printf("NO\n")
-#define one printf("1\n")
-#define _one printf("-1\n")
-#define zero printf("0\n")
-#define True printf("true\n")
-#define False printf("false\n")
-
-#define sortV(v) sort(v.begin(), v.end())
-#define sortR(v) sort(v.rbegin(), v.rend())
-#define Hacked return 0
-#define loop(i, s, n) for (int i = s; i < n; i++)
-#define rloop(i, n, s) for (int i = n - 1; i >= s; i--)
-
-#define scan(x, s)              \
-    for (int i = 0; i < s; i++) \
-        cin >> x[i];
-#define print(x)              \
-    for (auto value : x)      \
-        cout << value << " "; \
-    cout << endl;
-
 const int N = 1e7 + 10;
 long long int ar[N];
 
@@ -47,7 +22,7 @@ void Hack()
         ar[a] += d;
         ar[b + 1] -= d;
     }
-    loop(i, 1, n + 1)
+    for (int i = 1; i <= n; i++)
         ar[i] += ar[i - 1];
 
     long long mx = -1;
@@ -62,5 +37,5 @@ void Hack()
 int main()
 {
     Hack();
-    Hacked;
+    return 0;
 }
